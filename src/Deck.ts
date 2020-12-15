6566c3cd4682ae72758820a360dd248a0ac7d1ca
Derek Miranda <derek.miranda.1992@gmail.com>
2020-12-14 18:23:11 -0800

    src/Deck.ts Deck skeleton

--- src/Deck.ts
+++ src/Deck.ts
@@ -1,1 +1,16 @@
-export class Deck {}
+import { Card, Suit, FaceValue } from "./types";
+
+export class Deck {
+  protected _cards: Card[];
+
+  get cards() {
+    return this._cards;
+  }
+  set cards(_) {}
+
+  draw(n: number): Card[] {
+    return [];
+  }
+
+  shuffle(n: number): void {}
+}

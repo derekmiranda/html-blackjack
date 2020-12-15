75918be1ce4f22df123f71eca1eaa473de6cab71
Derek Miranda <derek.miranda.1992@gmail.com>
2020-12-14 19:23:26 -0800

    Adds draw method

--- src/Deck.spec.ts
+++ src/Deck.spec.ts
@@ -1,21 +1,78 @@
 import { Deck } from "./Deck";
 import { FACE_ORDER, SUIT_ORDER } from "./CONSTS";
+import { Card, FaceValue, Suit } from "./types";
 
 describe("Deck", () => {
+  describe("Drawing cards", () => {
+    test("Drawing 1 card at a time", () => {
+      const deck = new Deck();
+      expect(deck.draw(1)).toStrictEqual([
+        {
+          suit: Suit.Clubs,
+          value: FaceValue.Ace,
+        },
+      ]);
+      expect(deck.draw(1)).toStrictEqual([
+        {
+          suit: Suit.Clubs,
+          value: FaceValue.Two,
+        },
+      ]);
+    });
+
+    test("Drawing no cards", () => {
+      const deck = new Deck();
+      expect(deck.draw(0)).toStrictEqual([]);
+    });
+
+    test("Drawing multiple cards", () => {
+      const deck = new Deck();
+      expect(deck.draw(2)).toStrictEqual([
+        {
+          suit: Suit.Clubs,
+          value: FaceValue.Ace,
+        },
+        {
+          suit: Suit.Clubs,
+          value: FaceValue.Two,
+        },
+      ]);
+    });
+
+    test("Drawing more cards than deck has", () => {
+      const deck = new Deck();
+      // take out 51 cards
+      for (let i = 0; i < 51; i++) {
+        deck.draw(1);
+      }
+      expect(deck.hasCards).toBe(true);
+
+      // try taking 2 cards out
+      const drawnCards = deck.draw(2);
+      expect(drawnCards).toStrictEqual([
+        {
+          suit: Suit.Spades,
+          value: FaceValue.King,
+        },
+      ]);
+      expect(deck.hasCards).toBe(false);
+    });
+  });
+
   describe("Default state", () => {
-    const deck = new Deck();
+    const deck: Deck = new Deck();
+    const cards: Card[] = [];
+
+    while (deck.hasCards) {
+      cards.push(deck.draw(1)[0]);
+    }
 
     test("Deck should have 52 cards", () => {
-      expect(deck.cards.length).toBe(52);
+      expect(cards.length).toBe(52);
     });
 
     test("Cards should be ordered by suit (alphabetical)", () => {
-      const firstCardsOfEachSuit = [
-        deck.cards[0],
-        deck.cards[13],
-        deck.cards[26],
-        deck.cards[39],
-      ];
+      const firstCardsOfEachSuit = [cards[0], cards[13], cards[26], cards[39]];
       expect(firstCardsOfEachSuit.map((card) => card.suit)).toStrictEqual(
         SUIT_ORDER
       );
@@ -24,7 +81,7 @@ describe("Deck", () => {
     test("Cards should be ordered by face value within each suit", () => {
       for (let i = 0; i < 4; i++) {
         const offset = i * 13;
-        const cardsOfSuit = deck.cards.slice(offset, offset + 13);
+        const cardsOfSuit = cards.slice(offset, offset + 13);
         expect(cardsOfSuit.map((card) => card.value)).toStrictEqual(FACE_ORDER);
       }
     });
--- src/Deck.ts
+++ src/Deck.ts
@@ -2,27 +2,42 @@ import { Card } from "./types";
 import { SUIT_ORDER, FACE_ORDER } from "./CONSTS";
 
 export class Deck {
+  // stack: LIFO
   protected _cards: Card[] = [];
 
   get cards() {
     return this._cards;
   }
-  set cards(_) {}
+
+  get hasCards() {
+    return !!this._cards.length;
+  }
 
   constructor() {
     this.reset();
   }
 
   reset(): void {
-    for (let suit of SUIT_ORDER) {
-      for (let value of FACE_ORDER) {
-        this._cards.push({ suit, value });
+    // insert in reverse order
+    for (let i = SUIT_ORDER.length - 1; i >= 0; i--) {
+      for (let j = FACE_ORDER.length - 1; j >= 0; j--) {
+        this._cards.push({ suit: SUIT_ORDER[i], value: FACE_ORDER[j] });
       }
     }
   }
 
   draw(n: number): Card[] {
-    return [];
+    if (n < 1) return [];
+
+    const cardsToReturn: Card[] = [];
+    let i = 0;
+    while (this._cards.length && i < n) {
+      const card: Card = <Card>this._cards.pop();
+      card && cardsToReturn.push(card);
+      i++;
+    }
+
+    return cardsToReturn;
   }
 
   shuffle(n: number): void {}
